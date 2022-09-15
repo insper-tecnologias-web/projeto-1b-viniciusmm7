90fb2947f81ae3bae08ecb16f17b6f5721e06311
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-09-15 09:32:38 -0300

    parte 06 handout, fim

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 
 def index(request):
-    all_notes = Note.objects.all()
-    print(all_notes)
-    return render(request, 'notes/index.html', {'notes': all_notes})
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+
+        # Se estiver vazio, não adiciona no banco de dados
+        if title == '' or content == '':
+            return redirect('index')
+
+        note = Note(title=title, content=content)
+        note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})

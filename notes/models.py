e17cb32e490002b1499ef44b689649096e2bcb4c
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-09-15 08:51:49 -0300

    parte 04 handout

--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=400)
+
+    def __str__(self):
+        return str(self.title)

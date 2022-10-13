dc2e08a543ca7d6df732346f7bf0979af1395eb3
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-10-13 18:35:05 -0300

    CRUD implementado

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete', views.delete, name='delete'),
+    path('update', views.update, name='update')
 ]
--- notes/views.py
+++ notes/views.py
@@ -4,8 +4,8 @@ from .models import Note
 
 def index(request):
     if request.method == 'POST':
-        title = request.POST.get('titulo')
-        content = request.POST.get('detalhes')
+        title = request.POST.get('title')
+        content = request.POST.get('content')
         # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
 
         if not title or not content:
@@ -14,6 +14,31 @@ def index(request):
         note = Note(title=title, content=content)
         note.save()
         return redirect('index')
+
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request):
+    if request.method == 'POST':
+        id = request.POST.get('id-delete')
+
+        Note.objects.filter(id=id).delete()
+        return redirect('index')
+
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
+
+def update(request):
+    if request.method == 'POST':
+        id = request.POST.get('id-update')
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        Note.objects.filter(id=id).update(title=title, content=content)
+        return redirect('index')
+
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
